9e8f5e2f5a2b90074c6f2f932e1591a054f000fd
mdawso04 <mdawso04@gmail.com>
2021-10-06 06:12:27 +0900

    Added pycurl-requests

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,7 +1,41 @@
 import sys
 import os
-dirname = os.path.dirname(__file__)
-#filename = os.path.join(dirname, 'pykintone')
-sys.path.append(dirname)
 
+# add scripts folder to path
+# these are the main scripts
+scriptsDir = os.path.dirname(__file__)
+sys.path.append(scriptsDir)
+
+from configparser import ConfigParser
+_config = ConfigParser()
+_config.read('config.ini', encoding='utf_8')
+if 'pycurl_config' in _config.sections() and _config.getboolean('pycurl_config','use_pycurl'):
+    
+    # add scripts/pycurl-requests-0.2.1/pycurl_requests to path
+    # needed to replace 'requests' with 'pycurl' (required to access external SSPI/SSO services, ie kintone, from within corporate networks)
+    pcDir = os.path.join(scriptsDir, "pycurl-requests-0.2.1", "")
+    sys.path.append(pcDir)
+
+    # add scripts/pycurl-requests-0.2.1/pycurl_requests to path
+    # needed to set corporate network proxy settings for 'pycurl'
+    pcpDir = os.path.join(scriptsDir, "pycurl-requests-patch", "")
+    sys.path.append(pcpDir)
+
+    # Override requests - all requests calls will be replaced with pyCurl from now!
+    import pycurl_requests
+    import pycurl_requests_api_patch
+    pycurl_requests.patch_requests()
+    print("Requests patch complete!!")
+    import requests
+
+    # Additional - patch requests.api methods to include our corporate network proxy settings 
+    requests.request = pycurl_requests_api_patch.request
+    requests.head = pycurl_requests_api_patch.head
+    requests.get = pycurl_requests_api_patch.get
+    requests.post = pycurl_requests_api_patch.post
+    requests.put = pycurl_requests_api_patch.put
+    requests.patch = pycurl_requests_api_patch.patch
+    requests.delete = pycurl_requests_api_patch.delete
+
+# Load basic query API 'SOURCE'
 from .PQ import SOURCE
