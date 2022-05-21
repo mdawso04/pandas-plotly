960757777cfeeba8e6ed1a5e98f8dfb2a69b1ef4
mdawso <>
2022-05-21 17:15:43 +0900

    Fixn

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 setup(
-  name = 'pandas-plotly',
-  packages = ['pp'],
-  version = '0.1'
+    name = 'pandas-plotly',
+    packages = ['pp',],
+    version = '0.1',
+    long_description = 'A simple, unified interface for pandas & plotly for data wrangling, vizualization & report generating',
+    long_description_content_type = 'text/markdown',
 )
