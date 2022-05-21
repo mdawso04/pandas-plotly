2979e0b1bc7a4e8f257cc6964b1fcb78e1738c3c
mdawso <>
2022-05-21 16:49:40 +0900

    Fix2

--- setup.py
+++ setup.py
@@ -2,11 +2,5 @@ from distutils.core import setup
 setup(
   name = 'pandas-plotly',
   packages = ['pp'],
-  version = '0.1',
-  license='MIT',
-  description = 'Pandas & plotly wrapper for data wrangling & vizualization',
-  author = 'Michael Dawson',
-  author_email = 'mdawso04@gmail.com',
-  url = 'https://github.com/mdawso04/pandas-plotly',
-  keywords = ['pandas', 'plotly', 'vizualization'],
+  version = '0.1'
 )
