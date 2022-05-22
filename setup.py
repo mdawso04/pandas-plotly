e2a4ff3d3a8eb19618e8bf929830895bf61cb94b
mdawso <>
2022-05-22 09:21:05 +0900

    Commit

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version = '0.1',
     long_description = 'A simple, unified interface for pandas & plotly for data wrangling, vizualization & report generating',
     long_description_content_type = 'text/markdown',
+    install_requires=[
+        'numpy>=1.21.2',
+        'pandas>=1.3.47,
+        'plotly>=5.1.0',
+        'scikit_learn>=1.1.1',
+    ]
 )
