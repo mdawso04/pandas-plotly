befdd91104f669aa89f74ec5c389b75aa8c94ed4
mdawso <>
2022-08-08 06:30:28 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'pandas-plotly',
     packages = ['pp',],
-    version = '0.2.3',
+    version = '0.2.4',
     long_description = 'A simple, unified interface for pandas & plotly for data wrangling, vizualization & report generating',
     long_description_content_type = 'text/markdown',
     install_requires=[
